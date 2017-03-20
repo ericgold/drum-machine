86f83f96c57db817648b404a9279c2273b04ce31
ericgold <egold05@gmail.com>
2017-03-19 18:37:41 -0700

    add Sound class

--- app.js
+++ app.js
@@ -44,6 +44,40 @@ oscillator.type == sine;
 oscillator.frequency.value = 140;
 oscillator.connect(context.destination);
 
+// create Sound class to enable multiple oscillator stops and starts
+
+class Sound {
+
+	constructor(context) {
+		this.context = context;
+	}
+
+	init() {
+		this.oscillator = this.context.createOscillator();
+		this.gainNode = this.context.createGain();
+
+		this.oscillator.connect(this.gainNode);
+		this.gainNode.connect(this.context.destination);
+		this.oscillator.type = 'sine';
+	}
+
+	play(value, time) {
+		this.init();
+
+		this.oscillator.frequency.value = value;
+		this.gainNode.gain.setValueAtTime(1, this.context.currentTime);
+
+		this.oscillator.start(time);
+		this.stop(time);
+	}
+
+	stop(time) {
+		this.gainNode.gain.exponentialRampToValueAtTime(0.001, time + 1);
+		this.oscillator.stop(time + 1);
+	}
+	
+}
+
 function startOscillator() {
 	oscillator.start();
 }
